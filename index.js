680f42c4b1d85bc62c3f686475f921d920febe26
Lokeshwar28 <reddylokesh142@gmail.com>
2025-04-14 02:32:47 -0500

    updated cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(cors({
-  origin: [ 'http://localhost:5173', 'https://realtime-blog-frontend.vercel.app/' ],
+  origin: ['http://localhost:5173', 'https://realtime-blog-frontend.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
@@ -36,7 +36,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'https://realtime-blog-frontend.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://realtime-blog-frontend.vercel.app'],
     methods: ['GET', 'POST']
   }
 });
